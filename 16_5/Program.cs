e86322f966d44609508fcafc946e2cbc644c92d7
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-02-02 14:21:57 +0200

    1 задание

---
+++ 16_5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _16_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Количество посещений:");
+            int n = Convert.ToInt32(ReadLine());
+            Web_site[] website=new Web_site[n];
+            for (int i = 0; i < website.Length;i++)
+            {
+                Write("Введите адрес web-странички: ");
+                string address= ReadLine();
+                Write("Введите IP-адрес: ");
+                string ip = ReadLine();
+                Write("Введите браузер: ");
+                string browser = ReadLine();
+                Write("Введите дату посещения: ");
+                DateTime datevisit = DateTime.Parse(ReadLine());
+                Write("Введите время посещения: ");
+                DateTime timevisit = DateTime.Parse(ReadLine());
+                website[i] = new Web_site(address,ip,browser,datevisit,timevisit);
+            }
+            foreach(var elem in  website) 
+            {
+                elem.ShowInfo();
+            }
+            Write("Введите время вчерашего дня: ");
+            DateTime date1 = DateTime.Parse(ReadLine());
+            Write("Введите время сегодняшнего дня: ");
+            DateTime date2 = DateTime.Parse(ReadLine());
+            foreach (var elem in website)
+            {
+                if (date1 < elem.Timevisit && date2 > elem.Timevisit)
+                {
+                    elem.ShowInfo();
+                }
+            }
+            ReadLine();
+        }
+    }
+}
---
+++ 16_5/Web-site.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _16_5
+{
+    /// <summary>
+    /// Класс описывающий посещение сайта 
+    /// </summary>
+    class Web_site
+    {
+        /// <summary>
+        /// адрес открытой web-странички
+        /// </summary>
+        private string address;
+        public string Address { get => address; set => address = value; }
+        /// <summary>
+        /// ip-адрес посетителя;
+        /// </summary>
+        private string ip;
+        public string Ip { get => ip; set => ip = value; }
+        /// <summary>
+        ///  web-браузер посетителя
+        /// </summary>
+        private string browser;
+        public string Browser { get => browser; set => browser = value; }
+        /// <summary>
+        /// дата визита
+        /// </summary>
+        private DateTime datevisit;
+        public DateTime Datevisit { get => datevisit; set => datevisit = value; }
+        /// <summary>
+        /// время визита
+        /// </summary>
+        private DateTime timevisit;
+        public DateTime Timevisit { get => timevisit; set => timevisit = value; }
+        /// <summary>
+        /// Конструктор без параметров 
+        /// </summary>
+        public Web_site() { }
+        /// <summary>
+        /// Конструктор с параметрами 
+        /// </summary>
+        public Web_site(string address, string ip,string browser,DateTime datevisit, DateTime timevisit )
+        {
+            this.Address = address;
+            this.Ip = ip;   
+            this.Browser = browser;
+            this.Datevisit = datevisit;
+            this.Timevisit = timevisit;
+        }
+        public void ShowInfo()
+        {
+            WriteLine($"Адрес:{Address}\nIP-адрес:{Ip}\nБраузер:{Browser}\nДата визита:{Datevisit.ToString("D")}\nВремя визита:{Timevisit.ToString("T")}\n");
+        }
+       
+
+
+
+    }
+}
